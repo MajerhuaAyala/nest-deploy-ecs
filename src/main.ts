cd3185d9afd03384774a476a02ce97d24a208682
MarcelinoMajerhua <marcelino.majerhua.dev@gmail.com>
2024-07-30 09:54:27 -0500

    feat: add service to login to user v1

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(+(process.env.SERVER_PORT as unknown as number));
+  await app.listen(3000);
 }
 bootstrap();
